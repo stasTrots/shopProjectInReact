0cee98e5638023080d898b79cf7dd5ad2139251a
Trotsenko Stas <stas9n82@gmail.com>
2021-03-09 20:49:34 +0200

    react++

--- src/index.js
+++ src/index.js
@@ -5,12 +5,25 @@ import ReactDOM from 'react-dom';
 //   id:"test"
 // },"Hello world");
 
-// const h1 =<h1 id="test2">Hello world 2</h1>
+// JSX
+// const h1 = <h1 id="test2"> Hello world 2</h1>
+
+// const ul = <ul>
+//   <li>List item 1</li>
+//   <li>List item 2</li>
+// </ul>
+
+// const app=<div>
+//   {h1}
+//   {ul}
+//   {5+10}
+// </div>
+
 
 
 function App() {
     return (
-        <>
+        <React.Fragment>
             <h1>Hellow world</h1>
             <p>loraskdjlskjdslk aksjdaksjdk ajslkd jaslk jdalk kjsld ja dakslj lals jdla ksjd ;askld jassj 
                 kajsdlkasjdalskjdaskljdalkjdaslkdjslakd askdj askl ja
@@ -18,7 +31,7 @@ function App() {
             </p>
 
             
-        </>
+        </React.Fragment>
     )
 }
-ReactDOM.render(<App/>,getElementById("root"))
+ReactDOM.render(<App/>,document.getElementById("root"))
