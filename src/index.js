149f06dc6cd606d24173cb5a7dac2978e5748793
Trotsenko Stas <stas9n82@gmail.com>
2021-03-13 11:18:45 +0200

    27-2

--- src/index.js
+++ src/index.js
@@ -1,37 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App'
 
-// const h1 = React.createElement("h1",{
-//   id:"test"
-// },"Hello world");
-
-// JSX
-// const h1 = <h1 id="test2"> Hello world 2</h1>
-
-// const ul = <ul>
-//   <li>List item 1</li>
-//   <li>List item 2</li>
-// </ul>
-
-// const app=<div>
-//   {h1}
-//   {ul}
-//   {5+10}
-// </div>
-
-
-
-function App() {
-    return (
-        <React.Fragment>
-            <h1>Hellow world</h1>
-            <p>loraskdjlskjdslk aksjdaksjdk ajslkd jaslk jdalk kjsld ja dakslj lals jdla ksjd ;askld jassj 
-                kajsdlkasjdalskjdaskljdalkjdaslkdjslakd askdj askl ja
-                j alksjd lakjs dajdl
-            </p>
-
-            
-        </React.Fragment>
-    )
-}
 ReactDOM.render(<App/>,document.getElementById("root"))
