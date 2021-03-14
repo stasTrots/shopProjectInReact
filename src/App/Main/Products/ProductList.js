ff2ad89485ada10d8b74fd7a56ce113618a00243
Trotsenko Stas <stas9n82@gmail.com>
2021-03-14 11:56:33 +0200

    add product map

--- src/App/Main/Products/ProductList.js
+++ src/App/Main/Products/ProductList.js
@@ -1,43 +1,28 @@
 import React from 'react'
 import ProductListItem from "./ProductListItem"
+import products from './Products'
 
 const ProductList = () => {
     return (
         <>
             <h1 className="page-title">Product List</h1>
             <div className="row">
-                <div className="col-lg-6">
-                    <ProductListItem
-                        name="iPhone 8"
-                        type="phone"
-                        capacity="128"
-                        price="500"
-                    />
-                </div>
-                <div className="col-lg-6">
-                    <ProductListItem
-                        name="iPhone 12"
-                        type="phone"
-                        capacity="512"
-                        price="1500"
-                    />
-                </div>
-                <div className="col-lg-6">
-                    <ProductListItem
-                        name="iPhone X"
-                        type="phone"
-                        capacity="128"
-                        price="1000"
-                    />
-                </div>
-                <div className="col-lg-6">
-                    <ProductListItem
-                        name="iPhone XS"
-                        type="phone"
-                        capacity="64"
-                        price="900"
-                    />
-                </div>
+
+                {
+                    products.map(product => (
+                        <div className="col-lg-6">
+                            <ProductListItem
+                                name={product.name}
+                                type={product.type}
+                                capacity={product.capacity}
+                                price={product.price}
+                            />
+                        </div>
+                    ))
+                }
+
+                
+                
             </div>
         </>    
 
---
+++ src/App/Main/Products/Products.js
@@ -0,0 +1,30 @@
+
+
+const products = [
+    {
+        name:"iPhone 8",
+        type:"phone",
+        capacity:"128",
+        price:"500",
+    },
+    {
+        name:"iPhone 12",
+        type:"phone",
+        capacity:"512",
+        price:"1500",
+    },
+    {
+        name:"iPhone X",
+        type:"phone",
+        capacity:"128",
+        price:"1000",
+    },
+    {
+        name:"iPhone XS",
+        type:"phone",
+        capacity:"64",
+        price:"900",
+    },
+]
+
+export default products
