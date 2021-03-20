27c5d7bcdb16a51b1db912321ff8e88fcd1e3687
Trotsenko Stas <stas9n82@gmail.com>
2021-03-20 11:12:06 +0200

    string to number

--- src/App/Main/Products/ProductListItem.js
+++ src/App/Main/Products/ProductListItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import './ProductListItem.css'
 
 
@@ -34,6 +35,14 @@ const ProductListItem = ({
             <button className="btn-add-to-cart">Add to cart</button>
         </div>
     )
+}
+
+ProductListItem.propTypes = {
+    name: PropTypes.string,
+    description: PropTypes.string,
+    type: PropTypes.string,
+    capacity: PropTypes.number,
+    price: PropTypes.number
 }
 
 export default ProductListItem
