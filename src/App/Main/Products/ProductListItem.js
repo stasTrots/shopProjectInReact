70a5b7721addf94956b7c2449ad7b05d369272f7
Trotsenko Stas <stas9n82@gmail.com>
2021-03-21 11:11:09 +0200

    crate onIncr 2

--- src/App/Main/Products/ProductListItem.js
+++ src/App/Main/Products/ProductListItem.js
@@ -18,14 +18,11 @@ import './ProductListItem.css'
 
 class ProductListItem extends Component {
 
-    constructor() {
-        super();
-        this.onIncrementClick = this.onIncrementClick.bind(this)
-    }
+    
     state = {
         productCount:1,
     }
-    onIncrementClick() {
+    onIncrementClick = () => {
         this.setState((prevState) => ({
             productCount:prevState.productCount + 1
         }))
