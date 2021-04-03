b701502cb10960b46c0048aa455ade99631a6fc6
Trotsenko Stas <stas9n82@gmail.com>
2021-04-03 11:37:53 +0300

    products => Object

--- src/App/Header/Cart/Cart.js
+++ src/App/Header/Cart/Cart.js
@@ -3,10 +3,16 @@ import {keys} from "lodash"
 import './cart.css'
 import products from '../../Main/Products/Products'
 
+const productObject = products.reduce((obj,product) => ({
+    ...obj,
+    [product.id]:product
+}),{})
+
+console.log(productObject)
+console.log(products)
 
 const Cart = ({productsInCart}) => {
-    console.log(products)
-    console.log(productsInCart)
+ 
     return (
         <div className="cart text-center">
             {
