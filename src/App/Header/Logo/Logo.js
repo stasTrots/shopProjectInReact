1f7a45455ea3e2aff03028f321bcffcdc1b8c44d
Trotsenko Stas <stas9n82@gmail.com>
2021-04-04 11:04:24 +0300

    Logo link

--- src/App/Header/Logo/Logo.js
+++ src/App/Header/Logo/Logo.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import logo from './logo.png'
+import { Link } from 'react-router-dom'
 
 const Logo = () => {
     return (
         <div className="logo">
-            <img src={logo} alt=""/>
+             <Link to="/"><img src={logo} alt=""/></Link>
+            
         </div>
     )
 }
